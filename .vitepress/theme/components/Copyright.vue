989ae20dca3633e22842fe33fca0e793a3ab5fa1
Albert's Server(Ali) <albert990624@gmail.com>
2024-02-21 19:50:11 +0800

    feat: add unocss

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,29 +1,46 @@
 <template>
-    <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.0.0-rc.32</a>
-        Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
-    </div>
+	<div class="site-footer">
+		MIT Licensed | Copyright © 2021-2022
+		<a
+			class="vitepress"
+			:href="website"
+			>{{ webTitle }}</a
+		><br />
+		Powered by
+		<a
+			class="vitepress"
+			target="_blank"
+			href="//vitepress.vuejs.org/"
+			>VitePress - 1.0.0-rc.32</a
+		>
+		Theme by
+		<a
+			class="vitepress"
+			target="_blank"
+			href="//github.com/airene/vitepress-blog-pure"
+			>Vitepress-blog</a
+		>
+	</div>
 </template>
 <script lang="ts" setup>
-import { useData } from 'vitepress'
+import { useData } from "vitepress";
 
-const { site, theme } = useData()
-const website = theme.value.website
-const webTitle = site.value.title
+const { site, theme } = useData();
+const website = theme.value.website;
+const webTitle = site.value.title;
 </script>
 
 <style>
 .site-footer {
-    color: #888;
-    text-align: center;
-    font-size: 0.75rem;
-    width: 100%;
-    padding: 15px 0;
-    overflow: auto;
+	color: #888;
+	text-align: center;
+	font-size: 0.75rem;
+	width: 100%;
+	padding: 15px 0;
+	overflow: auto;
 }
 .vitepress {
-    color: var(--vp-c-text-1);
-    font-weight: 700;
+	color: var(--vp-c-text-1);
+	font-weight: 700;
 }
 </style>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -7,6 +7,7 @@ import Page from './components/Page.vue'
 import Comment from './components/Comment.vue'
 
 import './custom.css'
+import 'virtual:uno.css'
 
 export default {
     ...DefaultTheme,
